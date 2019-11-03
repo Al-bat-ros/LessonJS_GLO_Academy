798e5a97b583678d2bc27588cbe5001de90a5288
Aleksey Batnin <al-bat-ros@mail.ru>
2019-11-03 12:54:05 +0500

    Второе задание

---
+++ Lesson7/script/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let btnStart = document.getElementById('start');
+let plus = document.querySelectorAll('button');
+let plusIncome = plus[0];
+let plusExpenses= plus[1];
+let checkBox = document.querySelector('#deposit-check');
+let addIncome = document.querySelectorAll('.additional_income-item');
+let addIncome1 = addIncome[0];
+let addIncome2 = addIncome[1];
+let budget_month = document.querySelector('.budget_month-value');
+let budget_day = document.querySelector('.budget_day-value');
+let expenses_month = document.querySelector('.expenses_month-value');
+let additional_income = document.querySelector('.additional_income-value');
+let additional_expenses = document.querySelector('.additional_expenses-value');
+let income_period = document.querySelector('.income_period-value');
+let target_month = document.querySelector('.target_month-value');
+let exp_title = document.querySelectorAll('.expenses-title');
+let exp_title2 = exp_title[1];
+let exp_amount = document.querySelector('.expenses-amount'); 
+let addExpenens_item = document.querySelector('.additional_expenses-item');
+let target_amount = document.querySelector('.target-amount');
+let period_select = document.querySelector('.period-select');
+let period_amount = document.querySelector('.period-amount');
+
+
+
+console.log(btnStart);
+console.log(plusIncome);
+console.log(plusExpenses);
+console.log(checkBox);
+console.log(addIncome1);
+console.log(addIncome2);
+console.log(budget_month);
+console.log(budget_day);
+console.log(expenses_month);
+console.log(additional_income);
+console.log(additional_expenses);
+console.log(income_period);
+console.log(target_month);
+console.log(exp_title2);
+console.log(exp_amount);
+console.log(addExpenens_item);
+console.log(target_amount);
+console.log(period_select);
+console.log(period_amount);
+
